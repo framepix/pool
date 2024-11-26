516ae0325fc93a4175cea25d9c858aeff0e266c5
framepix <187810480+framepix@users.noreply.github.com>
2024-11-26 22:09:10 +0000

    added

---
+++ website_example/config.js
@@ -0,0 +1,21 @@
+var parentCoin = "GUUS";
+
+var api = "https://blockapi.guus.website";
+var poolHost = "guus.website";
+
+var email = "support@guus.website";
+var telegram = "https://t.me/Guus";
+var discord = "https://discord.gg/HpqF42znPm";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.guus.website/{symbol}/block/{id}";
+var transactionExplorer = "https://explorer.guus.website/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = "en";
+
+// Merged Mining:
+// var api = "http://poolhost/apiMerged";
+// var blockchainExplorer = "http://explorer.ird.cash/?hash={id}#block";
+// var transactionExplorer = "http://explorer.ird.cash/?hash={id}#transaction";
